75a4afd8223e21c900fee1fcb89a7bc183773728
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:52:45 -0300

    Update ex5_9.cpp

--- ex5_9.cpp
+++ ex5_9.cpp
@@ -1,3 +1,6 @@
+//Escrever um programa C para criar um vetor de inteiros de tamanho 10 e inicializar cada elemento com valores inteiros no intervalo [0,9]
+//(na declaracao) e exibir o conteudo do array na saıda padrao. Em seguida, multiplicar por 2 o conteudo de cada elemento e 
+//exibir novamente o vetor na saıda padrao.
 #include <stdio.h>
 #define SIZE 10
 int main() {
