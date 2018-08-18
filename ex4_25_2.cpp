8cf8a75f041be64744399eefd85b214d9694ee9d
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:36:24 -0300

    Update ex4_25_2.cpp

--- ex4_25_2.cpp
+++ ex4_25_2.cpp
@@ -1,3 +1,5 @@
+//Dado um pais A, com 5 milhoes de habitantes e uma taxa de natalidade de 3% ao ano, e um pais B com 7 milhoes de habitantes e uma taxa de natalidade de 2% ao ano, fazer um programa C para calcular e exibir na tela o tempo necessario para que a populacao 
+//do pais A ultrapasse a popula¸c˜ao do pais B.
 #include <stdio.h>
 int main(){
 	float a = 5000000;
