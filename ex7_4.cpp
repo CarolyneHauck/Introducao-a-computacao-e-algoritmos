6bf914324311b9e8f83fc5234c96d2c3d5acd1fe
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 15:03:02 -0300

    Update ex7_4.cpp

--- ex7_4.cpp
+++ ex7_4.cpp
@@ -1,3 +1,6 @@
+//Escreva uma funcao que calcule e retorne a media ponderada de 3 numeros inteiros fornecidos como argumentos de entrada. 
+//Os pesos devem ser 1, 2 e 3 respectivamente para o primeiro, segundo e terceiro numeros.
+
 #include <stdio.h>
 float media(int, int, int);
 int main() {
