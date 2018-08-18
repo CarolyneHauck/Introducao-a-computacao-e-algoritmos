d8ddc161aba5ab090374c43aef2d8a3ae5b0ccfa
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:38:23 -0300

    Update ex4_33.cpp

--- ex4_33.cpp
+++ ex4_33.cpp
@@ -1,3 +1,5 @@
+//Escrever um programa C que solicite ao usuario a digitacao em tempo de execucao de diversos numeros inteiros no intervalo [1,1000]. O ﬂag e qualquer valor fora desse intervalo. 
+//Apresentar na tela o maior e o menor valor valido – ou seja, dentro do intervalo – digitado pelo usuario.
 #include <stdio.h>
 int main(){
 	int i;
