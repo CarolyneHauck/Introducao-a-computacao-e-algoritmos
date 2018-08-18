50d330b176ad49c09b23d066360c6f971074bb8f
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:32:57 -0300

    Update ex4_6.cpp

--- ex4_6.cpp
+++ ex4_6.cpp
@@ -1,3 +1,5 @@
+//Escrever uma programa C que exiba na tela em ordem crescente todos os numeros inteiros compreendidos entre um limite inferior e um
+//limite superior fornecidos pelo usuario em tempo de execcao. Observe que o usuario pode digitar primeiro o limite superior.
 #include <stdio.h>
 int main() {
 	int i;
