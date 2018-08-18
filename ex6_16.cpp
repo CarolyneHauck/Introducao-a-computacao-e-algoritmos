3a751ab6b1744689d685190a79f4092f8fc56c0f
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:59:50 -0300

    Update ex6_16.cpp

--- ex6_16.cpp
+++ ex6_16.cpp
@@ -1,3 +1,10 @@
+//Gosto Nao Se Discute (junho/2010) Seu objetivo nessa questao consiste em gerar e exibir uma frase aleatoria em uma unica linha na tela.
+//A frase deve conter tres elementos: sujeito, verbo e predicado, nessa ordem. O sujeito deve ser escolhido randomicamente pelo
+//programa entre 4 opcoes possıveis: Pedro, Maria, Carlos e Ana. Analogamente, o verbo deve ser escolhido aleatoriamente pelo programa 
+//entre gosta de, nao gosta de, adora e detesta. Por ﬁm, o predicado deve ser escolhido randomicamente pelo programa entre feijoada, 
+//futebol, samba e feriado. Exemplo de execucao:
+//na detesta feriado.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
