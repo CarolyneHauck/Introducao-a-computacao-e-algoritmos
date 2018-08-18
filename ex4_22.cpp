f50d881b9130b0168cab6b04df76aeb66db9658d
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:35:04 -0300

    Update ex4_22.cpp

--- ex4_22.cpp
+++ ex4_22.cpp
@@ -1,3 +1,5 @@
+//Escrever um programa C que solicite ao usuario a digitacao em tempo de execucao de diversos caracteres. 
+//A entrada de dados deve terminar quando o usuario digitar um ponto. Exibir na tela o total de caracteres digitados.
 #include <stdio.h>
 int main(){
 	char c;
