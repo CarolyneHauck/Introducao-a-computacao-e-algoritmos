c39b4db747c2bffad4f5e558a6e3267f7d2068bd
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:51:23 -0300

    Update ex5_6.cpp

--- ex5_6.cpp
+++ ex5_6.cpp
@@ -1,3 +1,5 @@
+//Escrever um programa C para criar um vetor de inteiros de tamanho 18, inicializar cada elemento de forma que o conteudo do elemento seguinte seja igual ao conteu´do do elemento anterior adicionado de 2. O conteu´do do primeiro elemento do vetor deve ser fornecido pelo usuario em tempo de execucao. 
+//Exibir o conteudo do vetor na saıda padrao.
 #include <stdio.h>
 int main() {
 	int i;
