bf7a28ae3c8629dacb56e4ddc4bf15a946bdc0a5
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:47:58 -0300

    Update ex5_25.cpp

--- ex5_25.cpp
+++ ex5_25.cpp
@@ -1,3 +1,5 @@
+//Escrever um programa C que inicialize randomicamente os elementos de uma matriz quadrada de ordem 10 de inteiros no intervalo [0,999]. 
+//Exibir na saıda padrao o ındice e o valor correspondente ao maior elemento da matriz.
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
