89a499e4e999127d87c6312c0ea545d3237701b6
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:41:49 -0300

    Update ex5_10.cpp

--- ex5_10.cpp
+++ ex5_10.cpp
@@ -1,3 +1,4 @@
+//Escrever um programa C para criar um vetor de inteiros de tamanho 20 cujo conteudo seja numeros pseudoaleatorios no intervalo [0,9].
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
