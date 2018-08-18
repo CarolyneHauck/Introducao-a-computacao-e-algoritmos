52ed53637e1c482394563ae2a0f491baafefeb6d
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:38:56 -0300

    Update ex4_34.cpp

--- ex4_34.cpp
+++ ex4_34.cpp
@@ -1,3 +1,6 @@
+//Escrever um programa C que solicite ao usuario a digitacao de diversos numeros inteiros e positivos. 
+//O programa deve veriﬁcar cada um dos numeros fornecidos e um quadrado perfeito. O ﬂag e um numero nao-positivo. 
+//Um numero e dito quadrado perfeito quando a raiz quadrada e um numero inteiro.
 #include <stdio.h>
 #include <math.h>
 int main(){
