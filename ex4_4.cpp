57f13aab08a89764386b2a7ecc282c9dbac8f85d
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:31:51 -0300

    Update ex4_4.cpp

--- ex4_4.cpp
+++ ex4_4.cpp
@@ -1,3 +1,5 @@
+//Escrever uma programa C para exibir na tela uma tabela com a temperatura em graus Celsius e a temperatura equivalente em Fahrenheit, para valores entre 15 e 35 graus Celsius. 
+//Formula de conversao: TF = 9TC/5 + 32
 #include <stdio.h>
 int main() {
 	float f;
