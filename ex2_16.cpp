2843ae0ca1be827655fd545a902326a87b80ea13
Carolyne Carreira <33502583+CarolyneHauck@users.noreply.github.com>
2018-08-18 14:21:18 -0300

    Update ex2_16.cpp

--- ex2_16.cpp
+++ ex2_16.cpp
@@ -1,3 +1,5 @@
+//Escreva um programa C que solicite ao usuario a digitacao de um numero real. O programa deve exibir na tela o valor inteiro mais proximo do numero real informado.
+//Por exemplo, se o numero digitado for 3.8, o valor inteiro mais proximo e 4.
 #include <stdio.h>
 int main() {
 	float f;
